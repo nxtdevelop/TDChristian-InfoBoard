20717d506647331a5bf2735137f221a73ecd5c76
Ethan <ethan@nxtdevelop.com>
2018-02-15 13:19:12 -0500

    Initial Commit

---
+++ TDChristian InfoBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDChristian InfoBoard
+//
+//  Created by Ethan Vander Kooi on 2018-02-15.
+//  Copyright © 2018 Next Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
